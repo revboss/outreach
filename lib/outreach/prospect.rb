953826e962b5d4b20a0c13175de1867ecba8733a
Michael Novi <mike@revboss.com>
2016-01-29 15:03:50 -0500

    Removed debug statements

--- lib/outreach/prospect.rb
+++ lib/outreach/prospect.rb
@@ -6,7 +6,6 @@ module Outreach
         headers: @headers
       }
 
-      byebug
       response = self.class.post('/1.0/prospects', options)
       if response.code != 200
         raise "Unexpected response for Outreach prospect creation (#{response.code}): #{response.message} "
